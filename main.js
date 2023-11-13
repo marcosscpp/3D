666355f721685ab7ab6f94831d76ebb6cd2d0038
Marcos-psr <110942404+Marcos-psr@users.noreply.github.com>
2023-11-13 15:36:14 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,88 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x444444);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+);
+camera.position.set(4, 5, 11);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 5;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
+
+
+const spotLight = new THREE.SpotLight(0xffffff, 2000, 300, 1, 0.5, 2);
+spotLight.position.set(0, 50, 0);
+spotLight.castShadow = true;
+
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+
+spotLight.shadow.camera.near = 10;
+spotLight.shadow.camera.far = 200;
+spotLight.shadow.camera.fov = 30;
+scene.add(spotLight);
+
+const loader = new GLTFLoader().setPath("assets/");
+loader.load(
+  "scene.gltf",
+  (gltf) => {
+    const mesh = gltf.scene;
+
+    mesh.traverse((child) => {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+
+    mesh.position.set(0, 1.05, -1);
+    scene.add(mesh);
+
+    document.getElementById("progress-container").style.display = "none";
+  },
+  (xhr) => {
+    document.getElementById("progress").innerHTML = `LOADING ${
+      Math.max(xhr.loaded / xhr.total, 1) * 100
+    }/100`;
+  }
+);
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
